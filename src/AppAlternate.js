9c100b4b9a010c4c8def1e3a209711ce3bd6f4ac
Anthony <amaddatu@gmail.com>
2017-07-05 10:14:26 -0400

    Add react-router-dom prototype
    This is a working prototype with react-router-dom

--- src/AppAlternate.js
+++ src/AppAlternate.js
@@ -3,23 +3,23 @@ import { Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class AppAlternate extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Alternate</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload!
+          To get started, edit <code>src/AppAlternate.js</code> and save to reload!
         </p>
-        <h1><a href="/api/hello"> To test routing Click here! </a> </h1>
-        <p><a href="/api/blogposttest"> To test mongoDB Click here! </a> </p>
-        <Link to="/alternate" >Link to AppAlternate</Link>
+
+        <Link to="/" >Link to App</Link>
+        
       </div>
     );
   }
 }
 
-export default App;
+export default AppAlternate;
